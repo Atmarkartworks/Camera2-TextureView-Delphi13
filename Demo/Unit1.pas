6815fbc9e08c392fc57575df0d067afc9abb0ca6
Atmarkartworks <tmiura@atmarkartworks.com>
2026-05-09 12:36:11 +0900

    changing Unit1.pas

--- Demo/Unit1.pas
+++ Demo/Unit1.pas
@@ -14,10 +14,12 @@ unit Unit1;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, Permissions,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.ScrollBox, FMX.Memo, FMX.TabControl,
-  FMX.Objects, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts,
-  DW.Camera;
+  FMX.Objects, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.DialogService,
+  FMX.Memo.Types,
+  DW.Camera
+  ;
 
 type
   TForm1 = class(TForm)
@@ -31,12 +33,17 @@ type
     CaptureImage: TImage;
     procedure StartCameraButtonClick(Sender: TObject);
     procedure TabControlChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+
   private
     FCamera: TCamera;
     procedure CameraDetectedFacesHandler(Sender: TObject; const AImage: TBitmap; const AFaces: TFacesArray);
     procedure CameraStatusChangeHandler(Sender: TObject);
+    procedure DoActualRequest(const APermissions: TArray<string>; const AOnGranted: TProc);
+    procedure RequestCameraPermission;
+    procedure StartCameraPreview;
   public
-    constructor Create(AOwner: TComponent); override;
+    //constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
   end;
 
@@ -48,14 +55,50 @@ implementation
 {$R *.fmx}
 
 uses
-  FMX.Media;
+  FMX.Media;  // For TDevicePosition.Front / Back : insted of DW.Camera.Android
 
 const
   cStartCaptions: array[Boolean] of string = ('Start', 'Stop');
 
 { TForm1 }
 
-constructor TForm1.Create(AOwner: TComponent);
+// Standardize actual request processing
+procedure TForm1.DoActualRequest(const APermissions: TArray<string>; const AOnGranted: TProc);
+begin
+  PermissionsService.RequestPermissions(APermissions,
+    procedure(const APermissions: TClassicStringDynArray;
+              const AGrantResults: TClassicPermissionStatusDynArray)
+    var
+      LGranted: Boolean;
+      I: Integer;
+    begin
+      LGranted := Length(AGrantResults) = Length(APermissions);
+      if LGranted then
+      begin
+        for I := 0 to Length(AGrantResults) - 1 do
+        begin
+          if AGrantResults[I] <> TPermissionStatus.Granted then
+          begin
+            LGranted := False;
+            Break;
+          end;
+        end;
+      end;
+
+      if LGranted then
+      begin
+        if Assigned(AOnGranted) then
+          AOnGranted;
+      end
+      else
+      begin
+        TDialogService.ShowMessage('The CAMERA permission is required to launch the camera.');
+      end;
+    end);
+end;
+
+
+procedure TForm1.FormCreate(Sender: TObject);
 begin
   inherited;
   TabControl.ActiveTab := PreviewTab;
@@ -66,21 +109,64 @@ begin
   FCamera.OnDetectedFaces := CameraDetectedFacesHandler;
 end;
 
+
+
+procedure TForm1.RequestCameraPermission;
+var
+  Permissions: TArray<string>;
+  TargetPermission: string;
+begin
+  {$IFDEF ANDROID}
+  TargetPermission := 'android.permission.CAMERA';
+  Permissions := [TargetPermission];
+
+  if PermissionsService.IsPermissionGranted(TargetPermission) then
+    StartCameraPreview
+  else
+    DoActualRequest(Permissions, StartCameraPreview);
+  {$ELSE}
+  StartCameraPreview;
+  {$ENDIF}
+end;
+
+procedure TForm1.StartCameraPreview;
+begin
+  FCamera.PreviewControl.Parent := PreviewTab;
+  FCamera.Active := True;
+end;
+
+//constructor TForm1.Create(AOwner: TComponent);
+//begin
+//  inherited;
+//  TabControl.ActiveTab := PreviewTab;
+//  FCamera := TCamera.Create;
+//  FCamera.FaceDetectMode := TFaceDetectMode.Full;
+//  FCamera.CameraPosition := TDevicePosition.Front;
+//  FCamera.OnStatusChange := CameraStatusChangeHandler;
+//  FCamera.OnDetectedFaces := CameraDetectedFacesHandler;
+//end;
+
 destructor TForm1.Destroy;
 begin
-  FCamera.Free;
+(*  FCamera.Free; *)
+  FreeAndNil(FCamera);
   inherited;
 end;
 
 procedure TForm1.StartCameraButtonClick(Sender: TObject);
 begin
-  FCamera.PreviewControl.Parent := PreviewTab;
-  FCamera.Active := not FCamera.Active;
+  if FCamera.Active then
+  begin
+    FCamera.Active := False;
+    Exit;
+  end;
+  RequestCameraPermission;
 end;
 
 procedure TForm1.CameraStatusChangeHandler(Sender: TObject);
 begin
-  StartCameraButton.Text := cStartCaptions[FCamera.Active];
+  if Assigned(FCamera) and Assigned(FCamera.PreviewControl) then
+    StartCameraButton.Text := cStartCaptions[FCamera.Active];
 end;
 
 procedure TForm1.CameraDetectedFacesHandler(Sender: TObject; const AImage: TBitmap; const AFaces: TFacesArray);
@@ -105,10 +191,29 @@ begin
   TabControl.ActiveTab := CaptureTab;
 end;
 
+
+
+
+(*
+Crash before Forme Construction  
+*)
+
+//procedure TForm1.TabControlChange(Sender: TObject);
+//begin
+//  // This is necessary because the native preview will otherwise show over the top of the FMX controls
+//  FCamera.PreviewControl.Visible := TabControl.ActiveTab = PreviewTab;
+//end;
+
 procedure TForm1.TabControlChange(Sender: TObject);
 begin
   // This is necessary because the native preview will otherwise show over the top of the FMX controls
-  FCamera.PreviewControl.Visible := TabControl.ActiveTab = PreviewTab;
+  if Assigned(FCamera) and Assigned(FCamera.PreviewControl) then
+    FCamera.PreviewControl.Visible := TabControl.ActiveTab = PreviewTab;
 end;
 
+
+
+
+
 end.
+
